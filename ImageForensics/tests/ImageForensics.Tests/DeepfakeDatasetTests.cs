b93bae4d48a233081b1df06795354247f9c959b1
Matteo Polito <mapo80@gmail.com>
2025-07-28 19:31:32 +0200

    Add deepfake dataset test and results

---
+++ ImageForensics/tests/ImageForensics.Tests/DeepfakeDatasetTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ImageForensics.Core;
+using ImageForensics.Core.Models;
+using Xunit;
+
+namespace ImageForensics.Tests;
+
+public class DeepfakeDatasetTests
+{
+    private static readonly string DataDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../../", "dataset", "deepfake"));
+
+    [Fact]
+    public async Task Process_Deepfake_Dataset()
+    {
+        var analyzer = new ForensicsAnalyzer();
+        var results = new List<(string Type, string Name, double Score, long Ms)>();
+
+        foreach (string type in new[] { "real", "fake" })
+        {
+            var files = Directory.GetFiles(Path.Combine(DataDir, type), "*.jpg").Take(3);
+            foreach (var file in files)
+            {
+                string dir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Directory.CreateDirectory(dir);
+
+                var opts = new ForensicsOptions
+                {
+                    WorkDir = dir,
+                    NoiseprintMapDir = dir,
+                    NoiseprintModelsDir = AppContext.BaseDirectory
+                };
+
+                var sw = Stopwatch.StartNew();
+                var res = await analyzer.AnalyzeAsync(file, opts);
+                sw.Stop();
+
+                results.Add((type, Path.GetFileName(file), res.InpaintingScore, sw.ElapsedMilliseconds));
+
+                if (type == "real")
+                    res.InpaintingScore.Should().BeLessThan(0.35);
+                else
+                    res.InpaintingScore.Should().BeGreaterThan(0.20);
+            }
+        }
+
+        foreach (var r in results)
+            Console.WriteLine($"{r.Type};{r.Name};{r.Score:F3};{r.Ms}");
+
+        results.Should().NotBeEmpty();
+    }
+}
